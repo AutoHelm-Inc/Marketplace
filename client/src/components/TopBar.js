336786dcf6e010e87d6940831436fa4737d4c0f9
Zaid Omer <z2omer@uwaterloo.ca>
2024-02-21 18:57:00 -0500

    Fix some top bar ui

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -2,25 +2,33 @@ import React from "react";
 import "../App.css"
 import { Link } from "react-router-dom";
 import Logo from "../assets/AutoHelmLogo.png"
+import LogoWithName from "../assets/AutoHelmLogoWithName.png"
 import Explore from "../Explore";
 
 const TopBar = () => {
     return (
         <div>
             <div className="topBar">
-                <Link to="/" className="topBarLogoName">
-                    <div className="logo" style={{ marginLeft: 25 }}>
-                        <img src={Logo} style={{ width: '9vh', height: 'auto' }} alt="AutoHelm Logo" />
-                    </div>
-                    <p className="logoText">AutoHelm</p>
-                </Link>
+                <div className="logo" >
+                    <Link to="/" className="topBarLogoName">
+                        <img src={LogoWithName} style={{ width: '26vh', height: 'auto' }} alt="AutoHelm Logo" />
+                        {/* <img src={Logo} style={{ width: '9vh', height: 'auto' }} alt="AutoHelm Logo" /> */}
+                        {/* <p className="logoText">AutoHelm</p> */}
+                    </Link>
+                </div>
                 <div className="topBarLinksContainer">
-                    <Link to="/explore?q=" style={{ alignSelf: 'flex-start' }} className="topBarLinks">
-                        <p className="linkText">Explore</p>
-                    </Link>
-                    <Link to="/login" style={{ alignSelf: 'flex-start' }} className="topBarLinks">
-                        <p className="linkText">Login</p>
-                    </Link>
+
+                    <div style={{ paddingLeft: 100 }}>
+                        <Link to="/explore?q=" className="topBarLinks">
+                            <p className="linkText">Explore</p>
+                        </Link>
+                    </div>
+
+                    <div style={{ paddingLeft: 100 }}>
+                        <Link to="/login" className="topBarLinks">
+                            <p className="linkText">Login</p>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
